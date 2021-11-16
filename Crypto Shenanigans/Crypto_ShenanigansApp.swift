a7509e99a5eefa8f19bf2f9c4f44c2a1c98d8dc8
Sebastian Oberhauser <s.oberhauser@sap.com>
2021-11-15 21:57:08 -0800

    Initial Commit

---
+++ Crypto Shenanigans/Crypto_ShenanigansApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_ShenanigansApp.swift
+//  Crypto Shenanigans
+//
+//  Created by Oberhauser, Sebastian on 15.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_ShenanigansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
